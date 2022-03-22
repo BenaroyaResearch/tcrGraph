930b739e8eaeb4d3a207452a04f7ae9b82979990
Stephan Pribitzer <spribitzer@benaroyaresearch.org>
2022-03-22 15:01:20 -0700

    fix a logging bug

--- R/logging.R
+++ R/logging.R
@@ -6,17 +6,4 @@
   write(paste("Could not log event:", e), stderr())
 }
 
-.logEvent <- function(event){
-  tryCatch(
-    {
-      httr::POST(
-        url = "https://researchdb.benaroyaresearch.org/shineomatic/api/public/logging",
-        body = list(logData = list(logUser = Sys.info()[["user"]], event = event, app = "tcrGraph")),
-        encode = "json",
-        httr::timeout(0.7)
-      )
-    },
-    error = .loggingErrHandler,
-    warning = .loggingErrHandler
-  )
-}
+.logEvent = function(event){return(TRUE)},
